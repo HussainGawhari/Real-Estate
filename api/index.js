9d039757bcda0546d0b8cfcb5e0f6303e8072cee
HussainGawhari <hussain.gawhari2018@gmail.com>
2023-11-04 14:55:57 +0530

    Connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import  mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then( () => {
+    console.log('connect success');
+    }).catch( () => {
+        console.log(err);
+    });
+
 const app = express();
 app.listen(3000,() => {
 console.log('listening on port 3000');
